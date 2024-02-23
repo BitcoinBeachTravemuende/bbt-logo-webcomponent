2195b363d27e21006f3f72569d46f019bdf49dad
jk <email@jkrause.io>
2024-02-23 10:43:01 +0100

    custom elements manifest
    by following https://custom-elements-manifest.open-wc.org/

---
+++ custom-elements-manifest.config.mjs
@@ -0,0 +1,10 @@
+// Config
+// https://custom-elements-manifest.open-wc.org/analyzer/config/#config-file
+export default {
+  /** Globs to analyze */
+  globs: ['src/bbt-logo.ts'],
+  /** Directory to output CEM to */
+  outdir: '.',
+  /** Include third party custom elements manifests */
+  dependencies: false,
+}
